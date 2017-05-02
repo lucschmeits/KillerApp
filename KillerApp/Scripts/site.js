3dc4a808a1c6989f28e0301e1ff0fbada6edab96
lucschmeits <luc.06@live.nl>
2017-05-02 17:32:33 +0200

    added controllers, changed index, product view

--- KillerApp/Scripts/site.js
+++ KillerApp/Scripts/site.js
@@ -5,3 +5,15 @@
         $(this).find(".product-overlay").fadeOut();
     });
 });
+$(function () {
+    $('a[href="' + this.location.pathname + '"]').parent().addClass('active');
+    $('a[href="' + this.location.pathname + '"]').addClass("menu-icon-active");
+});
+$(function () {
+    $(".product").hover(function () {
+        $(this).find(".img-product").css("opacity", "0.3");
+    }, function () {
+        $(this).find(".img-product").css("opacity", "1");
+    }
+    );
+});
