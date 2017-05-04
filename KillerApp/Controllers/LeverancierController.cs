b5351e271b2f2c2b5bacd197af6f2b75302abd3c
lucschmeits <luc.06@live.nl>
2017-05-04 15:38:28 +0200

    added controller and view for shoppingcart

---
+++ KillerApp/Controllers/LeverancierController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KillerApp.Controllers
+{
+    public class LeverancierController : Controller
+    {
+        // GET: Leverancier
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
